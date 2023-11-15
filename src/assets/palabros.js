5a4d985d537d34c4de98b1366329d8ec0c6e8971
Ariel Fuentes <ariel.fuentesgarcia@gmail.com>
2023-11-14 21:12:48 -0600

    questions

--- src/assets/palabros.js
+++ src/assets/palabros.js
@@ -1,42 +1,63 @@
 export const PALABROS = [
   {
-    pregunta: 'Método especial para recorrer array',
-    palabro: 'FOREACH',
+    pregunta: '¿Cuál es el lugar más frío de la tierra?',
+    palabro: 'ANTARTIDA',
   },
   {
-    pregunta: 'Sustituto del condicional IF',
-    palabro: 'SWITCH',
+    pregunta: '¿Quién escribió La Odisea?',
+    palabro: 'HOMERO',
   },
   {
-    pregunta: 'Apellido del programador creador de JavaScript',
-    palabro: 'EICH',
+    pregunta: '¿Cuál es el río más largo del mundo?',
+    palabro: 'AMAZONAS',
   },
   {
-    pregunta: 'Propiedad que permite crear contenido HTML en una caja',
-    palabro: 'INNERHTML',
+    pregunta: '¿Cómo se llama la Reina del Reino Unido?',
+    palabro: 'ISABEL',
   },
   {
-    pregunta: 'Método que convierte un string en un array',
-    palabro: 'SPLIT',
+    pregunta: '¿En qué continente está Ecuador?',
+    palabro: 'AMERICA',
   },
   {
-    pregunta: 'Método que convierte un array en un texto',
-    palabro: 'JOIN',
+    pregunta: '¿Dónde originaron los juegos olímpicos?',
+    palabro: 'GRECIA',
   },
   {
-    pregunta: 'Canal de Youtube que hay que suscribirse si o si',
-    palabro: 'SOYJAB',
+    pregunta: '¿En qué país se encuentra la torre de Pisa?',
+    palabro: 'ITALIA',
   },
   {
-    pregunta: 'Propiedad que cuenta el número de caracteres de un string',
-    palabro: 'LENGTH',
+    pregunta: '¿Dónde se encuentra la Sagrada Familia?',
+    palabro: 'BARCELONA',
   },
   {
-    pregunta: 'Método que permite seleccionar una parte de un string',
-    palabro: 'SUBSTRING',
+    pregunta: '¿Cómo se denomina el resultado de la multiplicación?',
+    palabro: 'PRODUCTO',
   },
   {
-    pregunta: 'Método que permite unir dos arrays en uno',
-    palabro: 'CONCAT',
+    pregunta: '¿Cuál es el océano más grande?',
+    palabro: 'PACIFICO',
+  },
+  {
+    pregunta: '¿Cuál es el país más grande del mundo?',
+    palabro: 'RUSIA',
+  },
+  {
+    pregunta: '¿Dónde se encuentra la famosa Torre Eiffel?',
+    palabro: 'PARIS',
+  },
+  {
+    pregunta: '¿En qué lugar del cuerpo se produce la insulina?',
+    palabro: 'PANCREAS',
+  },
+  {
+    pregunta: '¿Qué rama de la Biología estudia los animales?',
+    palabro: 'ZOOLOGIA',
+  },
+  {
+    pregunta:
+      '¿De qué estilo arquitectónico es la Catedral de Notre Dame en París?',
+    palabro: 'GOTICO',
   },
 ];
